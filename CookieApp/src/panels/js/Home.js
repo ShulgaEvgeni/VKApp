84776c996c7b02107ed70e5098f52aae5deecc9a
ShulgaEvgeni <43778236+ShulgaEvgeni@users.noreply.github.com>
2018-12-10 13:48:55 +0700

    1

--- CookieApp/src/panels/js/Home.js
+++ CookieApp/src/panels/js/Home.js
@@ -1,23 +1,26 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Panel, ListItem, Button, Group, Div, Avatar, PanelHeader} from '@vkontakte/vkui';
+import {HeaderButton, platform, IOS} from '@vkontakte/vkui';
 import '../css/Home.css';
+import Icon28ChevronBack from '@vkontakte/icons/dist/28/chevron_back';
+import Icon24Back from '@vkontakte/icons/dist/24/back';
 
+const osname = platform();
 const Home = props => (
 	<Panel id={props.id}>
-		<PanelHeader>Example</PanelHeader>
+		<PanelHeader className="Header">
+			 Печенька Online
+		</PanelHeader>
 		<Div className="main">
-			<Div> 
-				<Button size="x1" level="1" onClick={props.go} data-to="persik">Show me the Persik, please</Button>
-			</Div>
-
-			<Div> 
-			
+			<Div className="Div-top"> 
+				<Button className="Button-top" size="x1" level="4" onClick={props.go} data-to="persik">Купить печеньки</Button>
+				<p> имя и какой-то текст</p>
 			</Div>
 
 			<Div className="Div-bottom"> 
-				<Button className="Button-bottom" size="xl" level="2" onClick={props.go} data-to="persik">Show me the Persik, please</Button>
-				<Button className="Button-bottom" size="xl" level="2" onClick={props.go} data-to="persik">Show me the Persik, please</Button>
+				<Button className="top" size="x1" level="2" onClick={props.go} data-to="persik">Разломить</Button>
+				<Button className="bottom" size="x1" level="2" onClick={props.go} data-to="persik">Отправиьт печеньку другу</Button>
 			</Div>	
 		</Div>
 	</Panel>
